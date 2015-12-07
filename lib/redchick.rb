45b5f3f58a93cd0c44ed9bdc18e8de18d97c7349
saki <sakihet@gmail.com>
2015-12-08 00:30:00 +0900

    add readline interface

--- lib/redchick.rb
+++ lib/redchick.rb
@@ -1,5 +1,20 @@
 require "redchick/version"
+require "readline"
 
 module Redchick
-  # Your code goes here...
+
+  class Cli
+    def start
+      puts "redchick version: #{Redchick::VERSION}"
+      while buf = Readline.readline("> ", true)
+        p buf
+      end
+    end
+  end
+
+  def self.start
+    cli = Redchick::Cli.new()
+    cli.start
+  end
+
 end
