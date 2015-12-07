0a4530980a5c394d5e51475053172bc84371c2a7
saki <sakihet@gmail.com>
2015-12-08 08:24:18 +0900

    generate config file

--- lib/redchick.rb
+++ lib/redchick.rb
@@ -1,11 +1,14 @@
 require "redchick/version"
 require "readline"
+require "yaml"
 require "oauth"
 require "twitter"
+require "pry"
 
 module Redchick
   CONSUMER_KEY = "6vSPn8nvt62lIXlmQ0f6JSI7O"
   CONSUMER_SECRET = "PYZQYeIm0ca6Jc6DMdlrMx0hfDyPKZPsksA1WvMWKtgzjeihSO"
+  CONFIG_FILE = ".redchick.yml"
 
   class Cli
     @client = nil
@@ -33,8 +36,12 @@ module Redchick
   end
 
   def self.start
-    access_token = get_access_token
-    cli = Redchick::Cli.new(access_token.token, access_token.secret)
+    generate_config_file unless File.exists? File.join(Dir.home, CONFIG_FILE)
+    @config = YAML.load_file(File.join(Dir.home, CONFIG_FILE))
+    current_user = @config[:current_user]
+    token = @config[:users][current_user.to_sym][:oauth_token]
+    secret = @config[:users][current_user.to_sym][:oauth_token_secret]
+    cli = Redchick::Cli.new(token, secret)
     cli.start
   end
 
@@ -51,4 +58,19 @@ module Redchick
     access_token = request_token.get_access_token(oauth_verifier: pin)
   end
 
+  def self.generate_config_file
+    access_token = get_access_token
+    screen_name = access_token.params[:screen_name]
+    config = {
+      current_user: screen_name,
+      users: {
+        "#{screen_name}": {
+                            oauth_token: access_token.token,
+                            oauth_token_secret: access_token.secret
+                          }
+      }
+    }
+    f = File.new(File.join(Dir.home, CONFIG_FILE), 'w')
+    f.write config.to_yaml
+  end
 end
