9499961651ebb451ee89e98dc4e64fa9a1ad8fbb
saki <sakihet@gmail.com>
2015-12-08 01:39:46 +0900

    add get_access_token method

--- lib/redchick.rb
+++ lib/redchick.rb
@@ -2,6 +2,8 @@ require "redchick/version"
 require "readline"
 
 module Redchick
+  CONSUMER_KEY = ""
+  CONSUMER_SECRET = ""
 
   class Cli
     def start
@@ -15,6 +17,19 @@ module Redchick
   def self.start
     cli = Redchick::Cli.new()
     cli.start
+  end
+
+  def get_access_token
+    consumer = OAuth::Consumer.new(
+      CONSUMER_KEY,
+      CONSUMER_SECRET,
+      site: 'https://api.twitter.com'
+    )
+    request_token = consumer.get_request_token
+    puts 'open following url and authorize it', request_token.authorize_url
+    puts 'enter PIN: '
+    pin = STDIN.gets.chomp
+    access_token = request_token.get_access_token(oauth_verifier: pin)
   end
 
 end
