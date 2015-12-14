254d0227a6dd7d19045191fa55587188d6e31d64
saki <sakihet@gmail.com>
2015-12-15 00:43:31 +0900

    add basic layout

--- lib/redchick/layout.rb
+++ lib/redchick/layout.rb
@@ -3,5 +3,12 @@ module Redchick
     def self.simple(t)
       "#{t.user.screen_name.rjust(15)}: #{t.text}"
     end
+
+    def self.basic(t)
+      "#{t.user.name} @#{t.user.screen_name} #{t.created_at}\n"\
+      "#{t.text}\n"\
+      "rt: #{t.retweet_count}, like: #{t.favorite_count}\n"\
+      "--\n"
+    end
   end
 end
