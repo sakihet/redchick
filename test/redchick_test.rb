6a805819819525d15daa13b2b42f1d7bd5271111
saki <sakihet@gmail.com>
2016-03-24 01:39:43 +0900

    fix version test

--- test/redchick_test.rb
+++ test/redchick_test.rb
@@ -2,10 +2,10 @@ require 'test_helper'
 
 class RedchickTest < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::Redchick::VERSION
+    refute_nil ::Redchick::VERSION::STRING
   end
 
   def test_version_number_format
-    assert_match(/(\d).(\d).(\d)/, Redchick::VERSION)
+    assert_match(/(\d).(\d).(\d)/, Redchick::VERSION::STRING)
   end
 end
